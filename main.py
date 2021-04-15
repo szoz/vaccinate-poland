7c4aa4f217a47bd643104a7caa50fc81157fc57d
Paweł Tymosiak <szozr2@gmail.com>
2021-04-15 12:59:59 +0200

    Added method endpoint Exercise 1.2 - added endpoint with request method feedback

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 
 app = FastAPI()
 
 
 @app.get('/')
 def read_root():
+    """Return dict with simple greeting."""
     return {'message': 'Hello world!'}
+
+
+@app.post('/method', status_code=201)
+@app.api_route('/method', methods=['GET', 'DELETE', 'PUT', 'OPTIONS'])
+def get_method(request: Request):
+    """Return dict with request method. Success status codes are handled by path decorators."""
+    return {'method': request.method}
--- test_main.py
+++ test_main.py
@@ -6,6 +6,25 @@ client = TestClient(app)
 
 
 def test_root():
-    response = client.get("/")
+    """Test '/' endpoint basic response."""
+    response = client.get('/')
+
     assert response.status_code == 200
     assert response.json() == {'message': 'Hello world!'}
+
+
+def test_method():
+    """Test status codes and returned methods in '/method' responses."""
+    test_path = '/method'
+    methods_to_status = {
+        'GET': 200,
+        'POST': 201,
+        'DELETE': 200,
+        'PUT': 200,
+        'OPTIONS': 200
+    }
+    responses = [client.request(method, test_path) for method in methods_to_status.keys()]
+
+    for case, response in zip(methods_to_status.items(), responses):
+        assert response.status_code == case[1]
+        assert response.json().get('method') == case[0]
