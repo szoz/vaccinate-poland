74d2b76243306bbfdcc925d328d5581771bba29e
Paweł Tymosiak <szozr2@gmail.com>
2021-04-15 14:50:03 +0200

    Added auth endpoint Exercise 1.3 - added endpoint with password hash validation

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, Response
+from hashlib import sha512
 
 app = FastAPI()
 
@@ -14,3 +15,11 @@ def read_root():
 def get_method(request: Request):
     """Return dict with request method. Success status codes are handled by path decorators."""
     return {'method': request.method}
+
+
+@app.get('/auth', status_code=204)
+def validates_password(password: str = '', password_hash: str = ''):
+    """Checks if provided password and password_hash match."""
+    password_encoded = password.encode('utf8')
+    if sha512(password_encoded).hexdigest() != password_hash:
+        return Response(status_code=401)
